045fc65d803d9b4764de287d8e140c386f57e212
mrkishanshrestha <kishanshrestha@hotmail.com>
2024-04-23 20:56:30 +0545

    asdasd

--- DASHBOARD/king.php
+++ DASHBOARD/king.php
@@ -1,6 +1,6 @@
 <?php 
 require_once '../SYSTEM/IMPORT/BACKEND/kali.php';
-$kali->isNotLoggedIn('https://www.systemic.com');
+$kali->isNotLoggedIn('https://kishan.vantageloaf.work');
 ?>
 
 <!DOCTYPE html>
@@ -30,32 +30,32 @@ $kali->isNotLoggedIn('https://www.systemic.com');
 
 
     if( $kali->sess('WELCOME_SOUND_PLAYED')==false && $kali->sess('USERKONAM')=='KING'){
-        echo'<script>juju.playSound("https://www.systemic.com/SYSTEM/IMPORT/SITE_DATA/SOUNDS/welcomekishan.mp3");</script>';
+        echo'<script>juju.playSound("https://kishan.vantageloaf.work/SYSTEM/IMPORT/SITE_DATA/SOUNDS/welcomekishan.mp3");</script>';
         $_SESSION['WELCOME_SOUND_PLAYED']=true;
     }
 
     $dataX=[
-        ['PLACE'=>'BODY','APP_NAME'=>'DASHBOARD','TITLE'=>'DASHBOARD','LINK'=>'https://www.systemic.com/DASHBOARD/maindash.php','ICON'=>'fa-brands fa-dashcube'],
+        ['PLACE'=>'BODY','APP_NAME'=>'DASHBOARD','TITLE'=>'DASHBOARD','LINK'=>$kali->domain.'/DASHBOARD/maindash.php','ICON'=>'fa-brands fa-dashcube'],
         
-        ['PLACE'=>'BODY','APP_NAME'=>'CLIENT','TITLE'=>'CLIENT','LINK'=>'https://www.systemic.com/ROOT/APPS/CLIENT/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'FACULTY','TITLE'=>'FACULTY','LINK'=>'https://www.systemic.com/ROOT/APPS/FACULTY/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'FACULTY_FINANCE','TITLE'=>'FACULTY FINANCE','LINK'=>'https://www.systemic.com/ROOT/APPS/FACULTY_FINANCE/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'COURSE','TITLE'=>'COURSE','LINK'=>'https://www.systemic.com/ROOT/APPS/COURSE/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'APPLICATION','TITLE'=>'APPLICATION','LINK'=>'https://www.systemic.com/ROOT/APPS/APPLICATION/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'COLLEGE','TITLE'=>'COLLEGE','LINK'=>'https://www.systemic.com/ROOT/APPS/COLLEGE/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'BRANCH','TITLE'=>'BRANCH','LINK'=>'https://www.systemic.com/ROOT/APPS/BRANCH/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'COURSE_ASSIGNMENT','TITLE'=>'COURSE ASSIGNMENT','LINK'=>'https://www.systemic.com/ROOT/APPS/COURSE_ASSIGNMENT/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'CLASSROOM','TITLE'=>'CLASSROOM','LINK'=>'https://www.systemic.com/ROOT/APPS/CLASSROOM/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'EXAMINATION','TITLE'=>'EXAMINATION','LINK'=>'https://www.systemic.com/ROOT/APPS/EXAMINATION/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'PAYMENT','TITLE'=>'PAYMENT','LINK'=>'https://www.systemic.com/ROOT/APPS/PAYMENT/CREATE','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'CLIENT','TITLE'=>'CLIENT','LINK'=>$kali->domain.'/ROOT/APPS/CLIENT/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'FACULTY','TITLE'=>'FACULTY','LINK'=>$kali->domain.'/ROOT/APPS/FACULTY/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'FACULTY_FINANCE','TITLE'=>'FACULTY FINANCE','LINK'=>$kali->domain.'/ROOT/APPS/FACULTY_FINANCE/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'COURSE','TITLE'=>'COURSE','LINK'=>$kali->domain.'/ROOT/APPS/COURSE/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'APPLICATION','TITLE'=>'APPLICATION','LINK'=>$kali->domain.'/ROOT/APPS/APPLICATION/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'COLLEGE','TITLE'=>'COLLEGE','LINK'=>$kali->domain.'/ROOT/APPS/COLLEGE/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'BRANCH','TITLE'=>'BRANCH','LINK'=>$kali->domain.'/ROOT/APPS/BRANCH/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'COURSE_ASSIGNMENT','TITLE'=>'COURSE ASSIGNMENT','LINK'=>$kali->domain.'/ROOT/APPS/COURSE_ASSIGNMENT/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'CLASSROOM','TITLE'=>'CLASSROOM','LINK'=>$kali->domain.'/ROOT/APPS/CLASSROOM/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'EXAMINATION','TITLE'=>'EXAMINATION','LINK'=>$kali->domain.'/ROOT/APPS/EXAMINATION/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'PAYMENT','TITLE'=>'PAYMENT','LINK'=>$kali->domain.'/ROOT/APPS/PAYMENT/CREATE','ICON'=>'fa-solid fa-book'],
         
-        ['PLACE'=>'BODY','APP_NAME'=>'PRIVILEGE','TITLE'=>'PRIVILEGE','LINK'=>'https://www.systemic.com/ROOT/APPS/PRIVILEGE/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'PRIVILEGE','TITLE'=>'PRIVILEGE','LINK'=>$kali->domain.'/ROOT/APPS/PRIVILEGE/MAIN','ICON'=>'fa-solid fa-book'],
         
-        ['PLACE'=>'BODY','APP_NAME'=>'USER','TITLE'=>'USER','LINK'=>'https://www.systemic.com/ROOT/APPS/USER/MAIN','ICON'=>'fa-solid fa-book'],
-        ['PLACE'=>'BODY','APP_NAME'=>'STUDENT','TITLE'=>'STUDENT','LINK'=>'https://www.systemic.com/ROOT/APPS/STUDENT/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'USER','TITLE'=>'USER','LINK'=>$kali->domain.'/ROOT/APPS/USER/MAIN','ICON'=>'fa-solid fa-book'],
+        ['PLACE'=>'BODY','APP_NAME'=>'STUDENT','TITLE'=>'STUDENT','LINK'=>$kali->domain.'/ROOT/APPS/STUDENT/MAIN','ICON'=>'fa-solid fa-book'],
 
-        ['PLACE'=>'FOOT','APP_NAME'=>'PROFILE','TITLE'=>'PROFILE','LINK'=>'https://www.systemic.com/ROOT/APPS/PROFILE','ICON'=>'fa-solid fa-user'],
-        ['PLACE'=>'FOOT','APP_NAME'=>'LOGOUT','TITLE'=>'LOGOUT','LINK'=>'https://www.systemic.com/logout.php','ICON'=>'fa-solid fa-arrow-right-from-bracket'],
+        ['PLACE'=>'FOOT','APP_NAME'=>'PROFILE','TITLE'=>'PROFILE','LINK'=>$kali->domain.'/ROOT/APPS/PROFILE','ICON'=>'fa-solid fa-user'],
+        ['PLACE'=>'FOOT','APP_NAME'=>'LOGOUT','TITLE'=>'LOGOUT','LINK'=>$kali->domain.'/logout.php','ICON'=>'fa-solid fa-arrow-right-from-bracket'],
     ];
 
     $kali->makeSideBar($dataX);
